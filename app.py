94f41ebfa45a17e28a77b1018da3a7bd56970d2d
rajitchaudhary <64368260+rajitchaudhary@users.noreply.github.com>
2024-09-15 19:29:46 +0530

    updated app.py

--- app.py
+++ app.py
@@ -88,5 +88,6 @@ def delete_faq(id):
     else:
         return jsonify({'error': 'FAQ not found'}), 404
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if _name_ == '_main_':
+        port = int(os.environ.get('PORT', 5000))
+            app.run(host='0.0.0.0', port=port, debug=True)
